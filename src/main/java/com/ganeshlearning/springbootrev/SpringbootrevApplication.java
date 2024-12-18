f3a542f5d399bdd49877f3278a61d8335eb13c52
Ganesh Sri Sai Pavan Kunapareddi <ganeshsrisaipavankunapareddi@Ganeshs-MacBook-Air.local>
2024-12-18 11:05:34 +0530

    project intial setup for revising spingboot micro services

---
+++ src/main/java/com/ganeshlearning/springbootrev/SpringbootrevApplication.java
@@ -0,0 +1,13 @@
+package com.ganeshlearning.springbootrev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootrevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootrevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ganeshlearning/springbootrev/SpringbootrevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganeshlearning.springbootrev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootrevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
